b9737da26bebeb3fe60edb76382d457d962b47d7
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-01-28 16:11:03 +0600

    Maven Project Created

---
+++ OpenCartProject/src/main/java/ocp/OpenCartProject/App.java
@@ -0,0 +1,13 @@
+package ocp.OpenCartProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
