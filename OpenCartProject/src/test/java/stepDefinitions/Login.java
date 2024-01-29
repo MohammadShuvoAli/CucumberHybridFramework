828223cb6a5633d50fcbcb93bdba16b5cf15de6d
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-01-29 22:23:06 +0600

    update

--- OpenCartProject/src/test/java/stepDefinitions/Login.java
+++ OpenCartProject/src/test/java/stepDefinitions/Login.java
@@ -9,56 +9,47 @@ public class Login {
 	
 	@Given("User has navigated to login page")
 	public void user_has_navigated_to_login_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
 	@When("User enters valid email address {string} into email field")
 	public void user_enters_valid_email_address_into_email_field(String emailText) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
 	@When("User enters valid password {string} into password field")
 	public void user_enters_valid_password_into_password_field(String passwordText) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
 	@When("User clicks on Login button")
 	public void user_clicks_on_login_button() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
 	@Then("User should get successfully logged in")
 	public void user_should_get_successfully_logged_in() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
 	@When("User has entered valid password {string} into password field")
 	public void user_has_entered_valid_password_into_password_field(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
 	@Then("User should get a proper warning message about credentials mismatch")
 	public void user_should_get_a_proper_warning_message_about_credentials_mismatch() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
 	@When("User dont enter any email address into email field")
 	public void user_dont_enter_any_email_address_into_email_field() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
 	@When("User dont enter password into password field")
 	public void user_dont_enter_password_into_password_field() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
 }
--- OpenCartProject/src/test/java/stepDefinitions/Search.java
+++ OpenCartProject/src/test/java/stepDefinitions/Search.java
@@ -8,44 +8,37 @@ public class Search {
 
 	@Given("User opens the Application")
 	public void user_opens_the_application() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
 	@When("User enters valid product {string} into Search box field")
 	public void user_enters_valid_product_into_search_box_field(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
 	@When("User clicks on Search button")
 	public void user_clicks_on_search_button() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
 	@Then("User should get valid product displayed in search results")
 	public void user_should_get_valid_product_displayed_in_search_results() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
 	@When("User enters invalid product {string} into Search box field")
 	public void user_enters_invalid_product_into_search_box_field(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
 	@Then("User should get a message about no product matching")
 	public void user_should_get_a_message_about_no_product_matching() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
 	@When("User dont enter any product name into Search box field")
 	public void user_dont_enter_any_product_name_into_search_box_field() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 	
 }
