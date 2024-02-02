92514ac340520ced8171c5dc06de957b19cd0b47
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-02-02 16:40:34 +0600

    search stepDefinition added

--- OpenCartProject/src/test/java/stepDefinitions/Search.java
+++ OpenCartProject/src/test/java/stepDefinitions/Search.java
@@ -1,5 +1,7 @@
 package stepDefinitions;
 
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import factory.DriverFactory;
@@ -20,32 +22,43 @@ public class Search {
 
 	@When("User enters valid product {string} into Search box field")
 	public void user_enters_valid_product_into_search_box_field(String validProductText) {
+		
+		driver.findElement(By.name("search")).sendKeys(validProductText);
 	    
 	}
 
 	@When("User clicks on Search button")
 	public void user_clicks_on_search_button() {
+		
+		driver.findElement(By.xpath("//button[@class='btn btn-default btn-lg']")).click();
 	    
 	}
 
 	@Then("User should get valid product displayed in search results")
 	public void user_should_get_valid_product_displayed_in_search_results() {
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//a[normalize-space()='HP LP3065']")).isDisplayed());
 	    
 	}
 
 	@When("User enters invalid product {string} into Search box field")
 	public void user_enters_invalid_product_into_search_box_field(String invalidProductText) {
 	    
+		driver.findElement(By.name(invalidProductText));
 	}
 
 	@Then("User should get a message about no product matching")
 	public void user_should_get_a_message_about_no_product_matching() {
+		
+		Assert.assertEquals("There is no product that matches the search criteria.", driver.findElement(By.xpath("//p[contains(text(),'There is no product that matches the search criter')]")));
 	    
 	}
 
 	@When("User dont enter any product name into Search box field")
 	public void user_dont_enter_any_product_name_into_search_box_field() {
 	    
+		// Intentionally Left Blank
+		
 	}
 	
 }
