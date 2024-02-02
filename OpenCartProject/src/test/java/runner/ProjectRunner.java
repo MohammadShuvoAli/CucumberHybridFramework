4e43e7454af0291264b1167fa6447a4eff930174
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-02-02 16:48:36 +0600

    runner implemented

--- OpenCartProject/src/test/java/runner/ProjectRunner.java
+++ OpenCartProject/src/test/java/runner/ProjectRunner.java
@@ -1,5 +1,15 @@
 package runner;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/features", glue= {"stepDefinations","hooks"})
+
 public class ProjectRunner {
+	
+	
 
 }
