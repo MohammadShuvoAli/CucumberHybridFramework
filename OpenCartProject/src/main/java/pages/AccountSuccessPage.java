4ff9babbfbfe62e33e07a391cbeeb7e169c610eb
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-02-07 18:43:55 +0600

    Update AccountSuccessPage.java

--- OpenCartProject/src/main/java/pages/AccountSuccessPage.java
+++ OpenCartProject/src/main/java/pages/AccountSuccessPage.java
@@ -5,26 +5,27 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import utils.ElementUtils;
+
 public class AccountSuccessPage {
 
 	WebDriver driver;
-
+	private ElementUtils elementUtils;
+	
 	public AccountSuccessPage(WebDriver driver) {
 
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
+		elementUtils = new ElementUtils(driver);
 
 	}
 
 	@FindBy(xpath = "//div[@id='content']/h1")
 	private WebElement pageHeading;
-	
 
 	public String getPageHeading() {
-
-		return pageHeading.getText();
+		
+		return elementUtils.getTextFromElement(pageHeading, 10);
+		
 	}
-	
-	
-
 }
