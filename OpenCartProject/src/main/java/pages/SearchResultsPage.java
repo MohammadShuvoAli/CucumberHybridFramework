6b28dfd10ad69ae0c801a1e630cc8da424e42eaa
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-02-07 18:53:11 +0600

    Update SearchResultsPage.java

--- OpenCartProject/src/main/java/pages/SearchResultsPage.java
+++ OpenCartProject/src/main/java/pages/SearchResultsPage.java
@@ -5,34 +5,38 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import utils.CommonUtils;
+import utils.ElementUtils;
+
 public class SearchResultsPage {
-
+	
 	WebDriver driver;
-
+	private ElementUtils elementUtils;
+	
 	public SearchResultsPage(WebDriver driver) {
-
+		
 		this.driver = driver;
-
-		PageFactory.initElements(driver, this);
-
+		PageFactory.initElements(driver,this);
+		elementUtils = new ElementUtils(driver);
+		
 	}
-
-	@FindBy(linkText = "HP LP3065")
+	
+	@FindBy(linkText="HP LP3065")
 	private WebElement validHPProduct;
-
-	@FindBy(xpath = "//p[contains(text(),'There is no product that matches the search criter')]")
+	
+	@FindBy(xpath="//input[@id='button-search']/following-sibling::p")
 	private WebElement messageText;
-
+	
 	public boolean displayStatusOfValidProduct() {
-
-		return validHPProduct.isDisplayed();
-
+		
+		return elementUtils.displayStatusOfElement(validHPProduct,CommonUtils.EXPLICIT_WAIT_BASIC_TIME);
+		
 	}
-
+	
 	public String getMessageText() {
-
-		return messageText.getText();
-
+		
+		return elementUtils.getTextFromElement(messageText,CommonUtils.EXPLICIT_WAIT_BASIC_TIME);
+		
 	}
 
 }
