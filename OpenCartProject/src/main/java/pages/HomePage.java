b7be944e6e33c79a8bfae26dd11db3f4f21135da
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-02-07 18:27:07 +0600

    ElementsUtils Completed

--- OpenCartProject/src/main/java/pages/HomePage.java
+++ OpenCartProject/src/main/java/pages/HomePage.java
@@ -5,14 +5,19 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import utils.ElementUtils;
+
 public class HomePage {
 
 	WebDriver driver;
+	private ElementUtils elementUtils;
 
 	public HomePage(WebDriver driver) {
 
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
+		elementUtils = new ElementUtils(driver);
+		
 	}
 
 	@FindBy(xpath = "//span[normalize-space()='My Account']")
@@ -31,11 +36,15 @@ public class HomePage {
 	private WebElement searchButton;
 
 	public void selectLoginOption() {
-		loginOption.click();
+		
+		elementUtils.clickOnElement(loginOption, 30);
+		
 	}
 
 	public void clickOnMyAccount() {
-		myAccountDropdown.click();
+		
+		elementUtils.clickOnElement(myAccountDropdown, 30);
+		
 	}
 
 	public void selectRegisterOption() {
